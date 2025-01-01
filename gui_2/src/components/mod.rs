218031c9ad192fa822e1df7a9ff9adca12afd463
pulgamecanica <pulgamecanica11@gmail.com>
2025-01-01 19:36:18 +0100

    remove unused module

--- gui_2/src/components/mod.rs
+++
@@ -1,7 +0,0 @@
-mod navbar;
-mod plane;
-mod sidebar;
-
-pub use navbar::Navbar;
-pub use plane::Plane;
-pub use sidebar::Sidebar;
--- gui_2/src/settings/mod.rs
+++
@@ -1,8 +0,0 @@
-mod grid_settings;
-mod plot_settings;
-mod sidebar_settings;
-
-pub use grid_settings::GridSettings;
-pub use plot_settings::PlotSettings;
-pub use sidebar_settings::SidebarSettings;
-pub use sidebar_settings::SidebarTab;
